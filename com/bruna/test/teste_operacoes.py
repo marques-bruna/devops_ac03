90944c1d39afbf4efacaa8d0769d12995e205418
marques-bruna <bruna.marques@aluno.faculdadeimpacta.com.br>
2020-10-08 17:31:13 -0300

    Configurado teste de funcao

--- com/bruna/test/teste_operacoes.py
+++ com/bruna/test/teste_operacoes.py
@@ -1,5 +1,11 @@
 def func(x):
-	return x + 1
+	return x + 8
 
-def test_answer():
-	assert func(3) == 5
+def test_positivo():
+	assert func(3) == 11
+    
+def test_negativo():
+	assert func(-15) == -7
+    
+ def test_zero():
+	assert func(0) == 8
